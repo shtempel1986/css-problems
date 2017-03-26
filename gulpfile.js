41ff171ed40369c5cc2ce2916102f50079f61e85
Pavel Ledovskikh <o_gredi@mail.ru>
2017-03-26 19:49:07 +0300

    решил первую задачку

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,6 @@ let gulp = require("gulp"), sass = require("gulp-sass"), browserSync = require("
 gulp.task("sass", () => {
     return gulp.src("default/sass/main.sass")
         .pipe(sass())
-        .pipe(autoprefixer([
-            'last 15 version',
-            '>1%',
-            'ie 8',
-            'ie 7'
-        ], {
-            cascade: true
-        }))
         .pipe(gulp.dest("default/css"));
 });
 
@@ -26,9 +18,23 @@ gulp.task("browser-sync", () => {
     });
 });
 
+gulp.task("autoprefixer", () => {
+    return gulp.src("default/css/main.css").
+        pipe(autoprefixer([
+            'last 15 version',
+            '>1%',
+            'ie 8',
+            'ie 7'
+        ], {
+            cascade: true
+        }))
+        .pipe(gulp.dest("default/css"));
+});
+
 gulp.task("watch", ["browser-sync", "sass"], () => {
     gulp.watch("default/sass/*.sass", ["sass", browserSync.reload]);
-    gulp.watch("default/*.html", browserSync.reload)
+    gulp.watch("default/*.html", browserSync.reload);
+    gulp.watch("default/css/*.css", ["autoprefixer", browserSync.reload]);
 });
 
 gulp.task("default",["watch"]);
