430348a6b7e0a658a0a101808016f1bbeb71bcf2
Pavel Ledovskikh <o_gredi@mail.ru>
2017-03-26 13:22:51 +0300

    Нача решать первую задачку

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Павел on 26.03.2017.
+ */
+let gulp = require("gulp"), sass = require("gulp-sass"), browserSync = require("browser-sync"), autoprefixer = require("gulp-autoprefixer");
+
+gulp.task("sass", () => {
+    return gulp.src("default/sass/main.sass")
+        .pipe(sass())
+        .pipe(autoprefixer([
+            'last 15 version',
+            '>1%',
+            'ie 8',
+            'ie 7'
+        ], {
+            cascade: true
+        }))
+        .pipe(gulp.dest("default/css"));
+});
+
+gulp.task("browser-sync", () => {
+    browserSync({
+        server:{
+            baseDir: "default"
+        },
+        notify: false
+    });
+});
+
+gulp.task("watch", ["browser-sync", "sass"], () => {
+    gulp.watch("default/sass/*.sass", ["sass", browserSync.reload]);
+    gulp.watch("default/*.html", browserSync.reload)
+});
+
+gulp.task("default",["watch"]);
